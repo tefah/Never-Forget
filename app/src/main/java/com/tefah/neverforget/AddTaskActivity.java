8d29f121f1b05fd22f414affa9200e55a416b6da
TEFA <mustafa.adel072@gmail.com>
2017-08-03 12:43:08 +0200

    initialRecordeAndImage

--- app/src/main/java/com/tefah/neverforget/AddTaskActivity.java
+++ app/src/main/java/com/tefah/neverforget/AddTaskActivity.java
@@ -1,31 +1,70 @@
 package com.tefah.neverforget;
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.tefah.neverforget.data.TaskContract;
 
+import java.io.File;
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class AddTaskActivity extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity implements MediaPlayer.OnSeekCompleteListener {
+
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+    public static final String FILE_PROVIDER_AUTHORITY = "com.tefah.fileprovider";
+
+
+    private String audioFileName;
+    private long date;
+    private Bitmap mResultsBitmap;
+    private String mTempPhotoPath;
 
     @BindView(R.id.addTask)
     Button addTask;
     @BindView(R.id.writtenNote)
     EditText textNote;
+    @BindView(R.id.imageNote)
+    ImageView imageNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("fileName")) {
+            audioFileName = intent.getStringExtra("fileName");
+        }
+        date = intent.getLongExtra("date", 0);
+        if (intent.getBooleanExtra("takePicture", false))
+            photoNote();
+
+
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,16 +72,115 @@ public class AddTaskActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * OnClick method for "camera note" Button. Launches the camera app.
+     */
+    @OnClick(R.id.photoNote)
+    public void photoNote() {
+        // Check for the external storage permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If you do not have permission, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Launch the camera if the permission exists
+            launchCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Called when you request permission to read and write to external storage
+        switch (requestCode) {
+            case REQUEST_STORAGE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // If you get permission, launch the camera
+                    launchCamera();
+                } else {
+                    // If you do not get permission, show a Toast
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    /**
+     * Creates a temporary image file and captures a picture to store in it.
+     */
+    private void launchCamera() {
+
+        // Create the capture image intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the temporary File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = Utilities.createTempImageFile(this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                // Get the path of the temporary file
+                mTempPhotoPath = photoFile.getAbsolutePath();
+
+                // Get the content URI for the image file
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+
+                // Add the URI so the camera can store the image
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Launch the camera activity
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the image capture activity was called and was successful
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Resample the saved image to fit the ImageView
+            mResultsBitmap = Utilities.resamplePic(this, mTempPhotoPath);
+            imageNote.setImageBitmap(mResultsBitmap);
+
+        } else {
+
+            // Otherwise, delete the temporary image file
+            Utilities.deleteImageFile(this, mTempPhotoPath);
+        }
+    }
+
     private void done(){
         String text = textNote.getText().toString();
         ContentValues values = new ContentValues();
         values.put(TaskContract.TaskEntry.COLUMN_TEXT, text);
-        values.put(TaskContract.TaskEntry.COLUMN_DATE, 1);
+        values.put(TaskContract.TaskEntry.COLUMN_DATE, date);
         values.put(TaskContract.TaskEntry.COLUMN_ALARM, 0);
 
         Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, values);
 
-        Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show();
+        Utilities.saveImage(this, mResultsBitmap);
         finish();
+    }
+
+
+    @Override
+    public void onSeekComplete(MediaPlayer mediaPlayer) {
+        Utilities.stopPlaying(mediaPlayer);
     }
 }
--- app/src/main/java/com/tefah/neverforget/MainActivity.java
+++ app/src/main/java/com/tefah/neverforget/MainActivity.java
@@ -1,12 +1,20 @@
 package com.tefah.neverforget;
 
+import android.Manifest;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,23 +23,53 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.tefah.neverforget.data.TaskContract;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static  int TASK_LOADER_ID = 0;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String audioFileName = null;
+    private TaskAdapter taskAdapter;
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
 
-    TaskAdapter taskAdapter;
 
     @BindView(R.id.tasksList)
     RecyclerView tasksList;
     @BindView(R.id.writeNote)
     FloatingActionButton writeNote;
+    @BindView(R.id.voiceNote)
+    FloatingActionButton voiceNote;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,13 +85,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
                 return false;
             }
-
             // Called when a user swipes left or right on a ViewHolder
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-
                 int id = (int) viewHolder.itemView.getTag();
-
                 // Build appropriate uri with String row id appended
                 String stringId = Integer.toString(id);
                 Uri uri = TaskContract.TaskEntry.CONTENT_URI;
@@ -69,14 +104,45 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
                 }
                 else if (motionEvent.getAction() == MotionEvent.ACTION_UP){
-                        Log.i("ACTIONdOWN", "ENOUGH IDIOT " );
                     addTask();
                 }
                 return true;
             }
         });
+        voiceNote.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+                switch (action){
+                    case MotionEvent.ACTION_DOWN:
+                        recorder = new MediaRecorder();
+                        audioFileName = Utilities.startRecording(recorder, MainActivity.this);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Utilities.stopRecording(recorder);
+                        Toast.makeText(MainActivity.this, audioFileName, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                        intent.putExtra("fileName", audioFileName);
+                        intent.putExtra("date", Utilities.getUnixTime());
+                        startActivity(intent);
+                        break;
+                    default:
+                }
+                return true;
+            }
+        });
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
     }
+
+@OnClick(R.id.photoNote)
+public void photoNote(){
+    Intent intent = new Intent(this, AddTaskActivity.class);
+    intent.putExtra("takePicture", true);
+    intent.putExtra("date", Utilities.getUnixTime());
+    startActivity(intent);
+}
 
     @Override
     protected void onResume() {
@@ -92,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     public void addTask(){
         Intent intent = new Intent(this,AddTaskActivity.class);
+        intent.putExtra("date", Utilities.getUnixTime());
         startActivity(intent);
     }
 
@@ -146,4 +213,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<Cursor> loader) {
         taskAdapter.swapCursor(null);
     }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(audioFileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+
 }
